102667f8c489052fe42d765492b055f01ee666b4
Shivashakthikumar17 <shvarumugam17@gmail.com>
2024-06-26 22:55:23 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import BookCrud from './BookCrud';
+
+function App() {
+  return (
+    <div className="App">
+    <BookCrud />
+    </div>
+  );
+}
+
+export default App;
